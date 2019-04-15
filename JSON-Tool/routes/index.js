fc5e587f7f5a2381abc94d2f251602e2786c9787
Alexjs95 <aj.scotson@hotmail.com>
2019-04-15 16:06:58 +0100

    Created generator html page and added custom css

--- JSON-Tool/routes/index.js
+++ JSON-Tool/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/generator', function(req, res, next) {
+  res.render('generator');
+});
+
 module.exports = router;
