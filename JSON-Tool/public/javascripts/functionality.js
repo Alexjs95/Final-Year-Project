86c0e91ebe1aad38a956452fcdc6df3eb5e44bf8
Alexjs95 <aj.scotson@hotmail.com>
2019-04-18 19:03:00 +0100

    Updated html combo box to have options for the 3 libraries, code is generated based on this selection. Added simple not empty validation on the combo box, desired element and json.

--- JSON-Tool/public/javascripts/functionality.js
+++ JSON-Tool/public/javascripts/functionality.js
@@ -36,146 +36,165 @@ window.onload = function(){
 
             lengthOfJSON = json.length;
 
+            if (!json) {
+                console.log("json invalid");
+            } else if (!language) {
+                console.log("language invalid");
+            } else if (!element) {
+                console.log("no element");
+            } else {
+
+                let elementPosition = json.search(element);
+                if (elementPosition > 0) {
+                    elementPosition = elementPosition + element.length;
+                    console.log("element found at position " + elementPosition);
+
+                    // Loops through the JSON file.
+                    for (i = 0; i < lengthOfJSON; i ++) {
+                        // checks the character and if its a curly bracket counts it as an object.
+                        if (json.charAt(i) == '{') {
+                            noOfObjects ++;
+                            if (i < elementPosition) {
+                                objectsBeforeElement ++;
+                            }
+                            // checks for square brackets and counts for array.
+                        } else if (json.charAt(i) == '[') {
+                            noOfArrays ++;
+                            if (i < elementPosition) {
+                                arraysBeforeElement ++;
+                            }
+                        }
+                    }
+                    // remove contents of json after the found element.
+                    json = json.substring(0, elementPosition);
+
+
+                    // determine whether the element is a key or a value.
+                    let elementType = "";
+                    if (json.lastIndexOf(":") > json.lastIndexOf(",")) {
+                        if (json.lastIndexOf("{") > json.lastIndexOf(":")) {
+                            elementType = "Key";
+                        } else {
+                            elementType = "Value";
+                        }
+                    } else {
+                        elementType = "Key";
+                    }
+
+                    console.log(json);
+                    console.log("element is a " + elementType);
+
+                    json = json.replace(/:/g, " ");
+                    json = json.replace(/,/g," ");
+                    json = json.replace(/"/g,"");
+                    json = json.replace(/\r\n|\n/g, "");
+                    json = json.replace(/ {1,}/g," ");
+
+                    console.log(json);
+
+                    // splits the json array by spaces.
+                    splitJSON = json.split(" ");
+
+                    let jsonSplit = new Array();
+                    // add the splitted json into an array.
+                    for (let i = 0; i < splitJSON.length; i++){
+                        jsonSplit.push(splitJSON[i]);
+                    }
+                    console.log(jsonSplit);
+
+                    let keyValues = new Array();
+                    let datatypes = new Array();
+                    for (let i = jsonSplit.length - 1; i >= 0; i--) {
+                        // last element in array.
+                        if (i === jsonSplit.length - 1) {
+                            // if element is a value then get item before from array.
+                            if (elementType == "Value") {
+                                // Check length of element to get relevant key.
+                                keyValues.push(jsonSplit[i - element.split(' ').length]);
+                                datatypes.push("Key");
+                            } else {
+                                keyValues.push(jsonSplit[i]);
+                                datatypes.push("Key");
+                            }
+                        }
+
+
+                        let valueRequired = true;
+
+                        // checks for beginning of object.
+                        if (jsonSplit[i] === "{") {
+                            // check everything after beginning of object.
+                            for (let j = i + 1; j < jsonSplit.length - 1; j++) {
+                                // if another object is initialed then break
+                                if (jsonSplit[j] === "{") {
+                                    break;
+                                    // if the object is closed then the current value of jsonSplit is not required.
+                                } else if (jsonSplit[j] === "}") {
+                                    valueRequired = false;
+                                    break;
+                                }
+                            }
+
+                            // Adds only required object values to array.
+                            if (i != 0 && valueRequired == true) {
+                                keyValues.push(jsonSplit[i - 1]);
+                                datatypes.push("Object");
+                            }
+
+                            // checks for beginning of Array.
+                        } else if (jsonSplit[i] === "[") {
+                            // check everything after beginning of array.
+                            for (let j = i + 1; j < jsonSplit.length - 1; j++) {
+                                // if another array is initialed then break
+                                if (jsonSplit[j] === "[") {
+                                    break;
+                                    // if the array is closed then the current value of jsonSplit is not required.
+                                } else if (jsonSplit[j] === "]") {
+                                    valueRequired = false;
+                                    break;
+                                }
+                            }
+
+                            // Adds only required array values to array.
+                            if (i != 0 && valueRequired == true) {
+                                keyValues.push(jsonSplit[i - 1]);
+                                datatypes.push("Array");
+                            }
+                        }
+
+                        if (i === 0) {
+                            if (jsonSplit[i] === "{") {
+                                datatypes.push("Object");
+                            } else if (jsonSplit[i] === "[") {
+                                datatypes.push("Array");
+                            }
+                            keyValues.push(jsonSplit[i]);
+                        }
+                    }
+
+                    console.log(keyValues);
+                    console.log(datatypes);
+
+                    let code;
+                    if (language === "org.json") {
+                        code = orgjson(keyValues, datatypes);
+                    } else if (language === "Jackson") {
+                        code = jackson(keyValues, datatypes);
+                    } else if (language === "gson") {
+                        code = gson(keyValues, datatypes);
+
+                    }
+
+                    document.getElementById('code-contents').value = code;
+                    console.log(code);
+                } else {
+                    console.log("element not found in json");
+                }
+            }
+
 
             // TODO: validation on JSON, Language and Element
 
-            let elementPosition = json.search(element);
-            if (elementPosition > 0) {
-                elementPosition = elementPosition + element.length;
-                console.log("element found at position " + elementPosition);
-
-                // Loops through the JSON file.
-                for (i = 0; i < lengthOfJSON; i ++) {
-                    // checks the character and if its a curly bracket counts it as an object.
-                    if (json.charAt(i) == '{') {
-                        noOfObjects ++;
-                        if (i < elementPosition) {
-                            objectsBeforeElement ++;
-                        }
-                    // checks for square brackets and counts for array.
-                    } else if (json.charAt(i) == '[') {
-                        noOfArrays ++;
-                        if (i < elementPosition) {
-                            arraysBeforeElement ++;
-                        }
-                    }
-                }
-                // remove contents of json after the found element.
-                json = json.substring(0, elementPosition);
-
-
-                // determine whether the element is a key or a value.
-                let elementType = "";
-                if (json.lastIndexOf(":") > json.lastIndexOf(",")) {
-                    if (json.lastIndexOf("{") > json.lastIndexOf(":")) {
-                        elementType = "Key";
-                    } else {
-                        elementType = "Value";
-                    }
-                } else {
-                    elementType = "Key";
-                }
-
-                console.log(json);
-                console.log("element is a " + elementType);
-
-                json = json.replace(/:/g, " ");
-                json = json.replace(/,/g," ");
-                json = json.replace(/"/g,"");
-                json = json.replace(/\r\n|\n/g, "");
-                json = json.replace(/ {1,}/g," ");
-
-                console.log(json);
-
-                // splits the json array by spaces.
-                splitJSON = json.split(" ");
-
-                let jsonSplit = new Array();
-                // add the splitted json into an array.
-                for (let i = 0; i < splitJSON.length; i++){
-                    jsonSplit.push(splitJSON[i]);
-                }
-                console.log(jsonSplit);
-
-                let keyValues = new Array();
-                let datatypes = new Array();
-                for (let i = jsonSplit.length - 1; i >= 0; i--) {
-                    // last element in array.
-                    if (i === jsonSplit.length - 1) {
-                        // if element is a value then get item before from array.
-                        if (elementType == "Value") {
-                            // Check length of element to get relevant key.
-                            keyValues.push(jsonSplit[i - element.split(' ').length]);
-                            datatypes.push("Key");
-                        } else {
-                            keyValues.push(jsonSplit[i]);
-                            datatypes.push("Key");
-                        }
-                    }
-
-
-                    let valueRequired = true;
-
-                    // checks for beginning of object.
-                    if (jsonSplit[i] === "{") {
-                        // check everything after beginning of object.
-                        for (let j = i + 1; j < jsonSplit.length - 1; j++) {
-                            // if another object is initialed then break
-                            if (jsonSplit[j] === "{") {
-                                break;
-                            // if the object is closed then the current value of jsonSplit is not required.
-                            } else if (jsonSplit[j] === "}") {
-                                valueRequired = false;
-                                break;
-                            }
-                        }
-
-                        // Adds only required object values to array.
-                        if (i != 0 && valueRequired == true) {
-                            keyValues.push(jsonSplit[i - 1]);
-                            datatypes.push("Object");
-                        }
-
-                        // checks for beginning of Array.
-                    } else if (jsonSplit[i] === "[") {
-                        // check everything after beginning of array.
-                        for (let j = i + 1; j < jsonSplit.length - 1; j++) {
-                            // if another array is initialed then break
-                            if (jsonSplit[j] === "[") {
-                                break;
-                                // if the array is closed then the current value of jsonSplit is not required.
-                            } else if (jsonSplit[j] === "]") {
-                                valueRequired = false;
-                                break;
-                            }
-                        }
-
-                        // Adds only required array values to array.
-                        if (i != 0 && valueRequired == true) {
-                            keyValues.push(jsonSplit[i - 1]);
-                            datatypes.push("Array");
-                        }
-                    }
-
-                    if (i === 0) {
-                        if (jsonSplit[i] === "{") {
-                            datatypes.push("Object");
-                        } else if (jsonSplit[i] === "[") {
-                            datatypes.push("Array");
-                        }
-                        keyValues.push(jsonSplit[i]);
-                    }
-                }
-
-                console.log(keyValues);
-                console.log(datatypes);
-
-                code = jackson(keyValues, datatypes);
-                document.getElementById('code-contents').value = code;
-                console.log(code);
-            } else {
-                console.log("element not found in json");
-            }
         }
         document.getElementById('generate-code').addEventListener('click', onclick);
     }());
