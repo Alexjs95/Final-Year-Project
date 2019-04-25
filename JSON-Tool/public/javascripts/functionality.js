0e3de685fddc769c5be6e062746845097be281e9
Alexjs95 <aj.scotson@hotmail.com>
2019-04-25 11:56:45 +0100

    Code is now generated based on how the user uploaded the JSON for file uploading and API requests

--- JSON-Tool/public/javascripts/functionality.js
+++ JSON-Tool/public/javascripts/functionality.js
@@ -37,6 +37,8 @@ window.onload = function(){
             let e = document.getElementById('language');
             let language = document.getElementById('language').options[e.selectedIndex].text;
             let element = document.getElementById('element').value;
+            let jsonLoc = document.getElementById('url').value;
+
             document.getElementById('errors').value = "";
 
             let noOfArrays = 0;
@@ -45,6 +47,10 @@ window.onload = function(){
             let objectsBeforeElement = 0;
 
             lengthOfJSON = json.length;
+
+            if (!jsonLoc) {
+                jsonLoc = "File";
+            }
 
             if (!json) {
                 console.log("json invalid");
@@ -116,7 +122,7 @@ window.onload = function(){
                     console.log(jsonSplit);
 
                     let keyValues = new Array();
-                    let datatypes = new Array();
+                    let dataTypes = new Array();
                     for (let i = jsonSplit.length - 1; i >= 0; i--) {
                         // last element in array.
                         if (i === jsonSplit.length - 1) {
@@ -124,10 +130,10 @@ window.onload = function(){
                             if (elementType === "Value") {
                                 // Check length of element to get relevant key.
                                 keyValues.push(jsonSplit[i - element.split(' ').length]);
-                                datatypes.push("Key");
+                                dataTypes.push("Key");
                             } else {
                                 keyValues.push(jsonSplit[i]);
-                                datatypes.push("Key");
+                                dataTypes.push("Key");
                             }
                         }
 
@@ -150,7 +156,7 @@ window.onload = function(){
                             // Adds only required object values to array.
                             if (i !== 0 && valueRequired === true) {
                                 keyValues.push(jsonSplit[i - 1]);
-                                datatypes.push("Object");
+                                dataTypes.push("Object");
                             }
 
                             // checks for beginning of Array.
@@ -170,30 +176,30 @@ window.onload = function(){
                             // Adds only required array values to array.
                             if (i !== 0 && valueRequired === true) {
                                 keyValues.push(jsonSplit[i - 1]);
-                                datatypes.push("Array");
+                                dataTypes.push("Array");
                             }
                         }
 
                         if (i === 0) {
                             if (jsonSplit[i] === "{") {
-                                datatypes.push("Object");
+                                dataTypes.push("Object");
                             } else if (jsonSplit[i] === "[") {
-                                datatypes.push("Array");
+                                dataTypes.push("Array");
                             }
                             keyValues.push(jsonSplit[i]);
                         }
                     }
 
                     console.log(keyValues);
-                    console.log(datatypes);
+                    console.log(dataTypes);
 
                     let code;
                     if (language === "JSON-Java") {
-                        code = orgjson(keyValues, datatypes);
+                        code = orgjson(keyValues, dataTypes, jsonLoc);
                     } else if (language === "Jackson") {
-                        code = jackson(keyValues, datatypes);
+                        code = jackson(keyValues, dataTypes, jsonLoc);
                     } else if (language === "gson") {
-                        code = gson(keyValues, datatypes);
+                        code = gson(keyValues, dataTypes, jsonLoc);
 
                     }
 
@@ -211,35 +217,67 @@ window.onload = function(){
         document.getElementById('generate-code').addEventListener('click', onclick);
     }());
 
-    function orgjson(keyValues, datatypes) {
-        let code = "InputStream is = new FileInputStream(\"/Users/alexscotson/Downloads/jsonexample.json\");";
-        code = code + "\nJSONTokener tokener = new JSONTokener(is);\n\n";
+    function orgjson(keyValues, dataTypes, jsonLoc) {
+        let code;
+        if (jsonLoc === "File") {
+            code = "InputStream is = new FileInputStream(\"/Users/alexscotson/Downloads/jsonexample.json\");";
+            code = code + "\nJSONTokener tokener = new JSONTokener(is);\n\n"
+
+            if (keyValues[keyValues.length - 1] === "{") {
+                code = code + "JSONObject " + keyValues[0] + " = new JSONObject(tokener);\n";
+                code = code + "System.out.println(" + keyValues[0];
+            } else {
+                code = code +"JSONArray " + keyValues[0] + " = new JSONArray(tokener);\n";
+                code = code + "System.out.println(" + keyValues[0];
+            }
+
+        } else {
+            // api code
+
+            code = "InputStream is = new URL(\"" + jsonLoc +"\").openStream();\n";
+            code = code + "StringBuilder response = new StringBuilder();\n";
+            code = code + "BufferedReader br = new BufferedReader(new InputStreamReader(is, Charset.forName(\"UTF-8\")));\n";
+            code = code + "String inputLine;\n";
+            code = code + "while ((inputLine = br.readLine()) != null)\n";
+
+            code = code + "\tresponse.append(inputLine);\n";
+
+            if (keyValues[keyValues.length - 1] === "{") {
+                code = code + "JSONObject " + keyValues[0] + " = new JSONObject(response.toString());\n";
+                code = code + "System.out.println(" + keyValues[0];
+            } else {
+                code = code + "JSONArray " + keyValues[0] + " = new JSONArray(response.toString());\n";
+                code = code + "System.out.println(" + keyValues[0];
+            }
+        }
+
+
 
         for (let i = keyValues.length - 1; i >= 0; i --) {
             // last element of array
             if (i === keyValues.length -1 ) {
-                if (keyValues[i] === "{") {
-                    code = code + "JSONObject " + keyValues[0] + " = new JSONObject(tokener);\n";
-                    code = code + "System.out.println(" + keyValues[0];
-                } else if (keyValues[i] === "[") {
-                    code = code +"JSONArray " + keyValues[0] + " = new JSONArray(tokener);\n";
-                    code = code + "System.out.println(" + keyValues[0];
-                }
-
-                // if (datatypes[i] === "Object") {
-                //     code = code + "\nJSONObject " + keyValues[0] + " = new JSONObject(tokener);\n";
+                // if (keyValues[i] === "{") {
+                //     code = code + "JSONObject " + keyValues[0] + " = new JSONObject(tokener);\n";
                 //     code = code + "System.out.println(" + keyValues[0];
-                // } else if (datatypes[i] === "Array") {
-                //     code = code +"\nJSONArray " + keyValues[0] + " = new JSONArray(tokener);\n";
+                // } else if (keyValues[i] === "[") {
+                //     code = code +"JSONArray " + keyValues[0] + " = new JSONArray(tokener);\n";
                 //     code = code + "System.out.println(" + keyValues[0];
                 // }
 
+                    // if (dataTypes[i] === "Object") {
+                    //     code = code + "\nJSONObject " + keyValues[0] + " = new JSONObject(tokener);\n";
+                    //     code = code + "System.out.println(" + keyValues[0];
+                    // } else if (dataTypes[i] === "Array") {
+                    //     code = code +"\nJSONArray " + keyValues[0] + " = new JSONArray(tokener);\n";
+                    //     code = code + "System.out.println(" + keyValues[0];
+                    // }
+
             } else {
-                if (datatypes[i] === "Object") {
+                if (dataTypes[i] === "Object") {
                     code = code + ".getJSONObject(\"" + keyValues[i] + "\")";
-                } else if (datatypes[i] === "Array") {
+                } else if (dataTypes[i] === "Array") {
                     code = code + ".getJSONArray(\"" + keyValues[i] + "\")";
-                } else if (datatypes[i] === "Key") {
+                } else if (dataTypes[i] === "Key") {
                     code = code + ".get(\"" + keyValues[i] + "\"));";
                 }
             }
@@ -248,13 +286,20 @@ window.onload = function(){
     }
 
 
-    function jackson(keyValues, datatypes) {
-        let code = "byte[] jsonData = Files.readAllBytes(Paths.get(\"/Users/alexscotson/Downloads/jsonexample.json\"));";
-        code = code + "\nJsonNode rootNode = new ObjectMapper().readTree(jsonData);\n\n";
+    function jackson(keyValues, dataTypes, jsonLoc) {
+        let code;
+        if (jsonLoc === "File") {
+            code = "byte[] jsonData = Files.readAllBytes(Paths.get(\"/Users/alexscotson/Downloads/jsonexample.json\"));";
+            code = code + "\nJsonNode rootNode = new ObjectMapper().readTree(jsonData);\n\n";
+        } else {
+            // api code
+            code = "URL url = new URL(\"" + jsonLoc +"\");\n";
+            code = code + "JsonNode rootNode = new ObjectMapper().readTree(url);\n\n"
+        }
 
         for (let i = keyValues.length - 1; i >= 0; i --) {
             if (i !== keyValues.length - 1) {
-                if (datatypes[i] === "Key") {
+                if (dataTypes[i] === "Key") {
                     if (keyValues.length === 2) {
                         code = code + "JsonNode " + keyValues[i] + " =  rootNode.get(\"" + keyValues[i] + "\");";
                     } else {
@@ -266,6 +311,7 @@ window.onload = function(){
                         code = code + "JsonNode " + keyValues[i] + " = rootNode.get(\"" + keyValues[i] + "\");\n"
                     } else {
                         code = code + "JsonNode " + keyValues[i] + " = " + keyValues[i + 1] + ".get(\"" + keyValues[i] + "\");\n"
+
                     }
 
                 }
@@ -273,26 +319,52 @@ window.onload = function(){
 
             }
         }
+        code = code + "\nSystem.out.println(" + keyValues[0] + ");\n"
+
         return code;
     }
 
 
-    function gson(keyValues, datatypes) {
-        let code =  "byte[] jsonData = Files.readAllBytes(Paths.get(\"/Users/alexscotson/Downloads/jsonexample.json\"));\n";
-        code = code + "String jsonStr = new String(jsonData);\n";
+    function gson(keyValues, dataTypes, jsonLoc) {
+        let code;
+        console.log("jsonloc "  +jsonLoc);
+        if (jsonLoc === "File") {
+            // From file
+            code =  "byte[] jsonData = Files.readAllBytes(Paths.get(\"/Users/alexscotson/Downloads/jsonexample.json\"));\n";
+            code = code + "String jsonStr = new String(jsonData);\n";
+
+            if (keyValues[keyValues.length - 1] === "{") {
+                code = code + "JsonObject jsonObj = new Gson().fromJson(jsonStr, JsonObject.class);\n\n";
+            } else {
+                code = code + "JsonArray jsonObj = new Gson().fromJson(jsonStr, JsonArray.class);\n\n";
+            }
+
+        } else {
+            // api code
+            code = "URL url = new URL(\"" + jsonLoc +"\");\n";
+            code = code + "URLConnection request = url.openConnection();\n";
+            code = code + "request.connect();\n\n";
+
+            code = code + "JsonElement root = new JsonParser().parse(new InputStreamReader((InputStream) request.getContent()));";
+            if (keyValues[keyValues.length - 1] === "{") {
+                code = code + "JsonObject jsonObj = root.getAsJsonObject();\n\n";
+            } else {
+                code = code + "JsonArray jsonObj = root.getAsJsonArray();\n\n";
+            }
+        }
 
         for (let i = keyValues.length - 1; i >= 0; i --) {
             if (i === keyValues.length - 1) {
-                if (keyValues[i] === "{") {
-                    code = code + "JsonObject jsonObj = new Gson().fromJson(jsonStr, JsonObject.class);\n\n";
-                } else if (keyValues[i] === "[") {
-                    code = code + "JsonArray jsonObj = new Gson().fromJson(jsonStr, JsonArray.class);\n\n";
-                }
+                // if (keyValues[i] === "{") {
+                //     code = code + "JsonObject jsonObj = new Gson().fromJson(jsonStr, JsonObject.class);\n\n";
+                // } else if (keyValues[i] === "[") {
+                //     code = code + "JsonArray jsonObj = new Gson().fromJson(jsonStr, JsonArray.class);\n\n";
+                // }
 
             } else if (i === keyValues.length - 2) {
-                if (datatypes[i] === "Object") {
+                if (dataTypes[i] === "Object") {
                     code = code + "JsonObject " + keyValues[i] + "Obj = (JsonObject) jsonObj.get(\"" + keyValues[i] + "\");\n";
-                } else if (datatypes[i] === "Array") {
+                } else if (dataTypes[i] === "Array") {
                     code = code + "JsonArray " + keyValues[i] + "Obj = (JsonArray) jsonObj.get(\"" + keyValues[i] + "\");\n";
                 }
 
@@ -300,15 +372,17 @@ window.onload = function(){
                     code = code + "String " + keyValues[i] + " = jsonObj.get(\"" + keyValues[i] + "\").toString();\n";
                 }
             } else {
-                if (datatypes[i] === "Object") {
+                if (dataTypes[i] === "Object") {
                     code = code + "JsonObject " + keyValues[i] + "Obj = (JsonObject) " + keyValues[i + 1] + "Obj.get(\"" + keyValues[i] + "\");\n";
-                } else if (datatypes[i] === "Array") {
+                } else if (dataTypes[i] === "Array") {
                     code = code + "JsonArray " + keyValues[i] + "Obj = (JsonArray) " + keyValues[i + 1] + "Obj.get(\"" + keyValues[i] + "\");\n";
-                } else if (datatypes[i] === "Key") {
+                } else if (dataTypes[i] === "Key") {
                     code = code + "String " + keyValues[i] + " = " + keyValues[i + 1] + "Obj.get(\"" + keyValues[i] + "\").toString();\n";
                 }
             }
         }
+        code = code + "\nSystem.out.println(" + keyValues[0] + ");\n"
+
         return code;
     }
 };
